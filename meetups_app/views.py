aca9b5fe6b578b1cb2aef17ead2a25206dc4ba68
Michał Hewelt <mhewelt9@gmail.com>
2022-11-24 20:53:36 +0100

    add html and css

--- meetups_app/views.py
+++ meetups_app/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'meetups_app/index.html')
+    meetups = [
+        {
+             'title': 'A first meetup',
+             'location': 'New York',
+             'slug': 'a-first-meetup'
+         },
+        {
+             'title': 'A second meetup',
+             'location': 'Milan',
+             'slug': 'a-second-meetup'
+        },
+    ]
+    return render(request, 'meetups_app/index.html', {
+        'show_meetups': True,
+        'meetups': meetups,
+    })
 
